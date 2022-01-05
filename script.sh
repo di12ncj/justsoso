a7554e58aa6993ffdd7f09a882ad1297f2ad4852
di12ncj <94455250+di12ncj@users.noreply.github.com>
2022-01-05 17:49:09 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,28 +10,29 @@ url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 for((i=0;i<num;i++))
 do
 case i in
-  0)[
-        curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
-        udata=$(sed -n '11p' temp.txt)
-        udata=${udata#*ptopid=}
-        udata=${udata%\"\}\}*}
-        ptopid="${udata%&*}"
-        sid="${udata#*&sid=}" #登录获取ptopid和sid
-        curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
-        curl -d "@myvs1.txt" -d "jingdu=115.041&weidu=35.768&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
-        udata=$(sed -n '24,26p' temp.txt)
-        echo "$udata" > result.html]
+0)[curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+  udata=$(sed -n '11p' temp.txt)
+  udata=${udata#*ptopid=}
+  udata=${udata%\"\}\}*}
+  ptopid="${udata%&*}"
+  sid="${udata#*&sid=}" #登录获取ptopid和sid
+  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs1.txt" -d "jingdu=115.041&weidu=35.768&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  udata=$(sed -n '24,26p' temp.txt)
+  echo "$udata" > result.html]
   ;;
-  1)[curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
-        udata=$(sed -n '11p' temp.txt)
-        udata=${udata#*ptopid=}
-        udata=${udata%\"\}\}*}
-        ptopid="${udata%&*}"
-        sid="${udata#*&sid=}" #登录获取ptopid和sid
-        curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
-        curl -d "@myvs2.txt" -d "jingdu=116.737&weidu=35.136&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
-        udata=$(sed -n '24,26p' temp.txt)
-        echo "$udata" >> result.html]
+1)[curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+   udata=$(sed -n '11p' temp.txt)
+   udata=${udata#*ptopid=}
+   udata=${udata%\"\}\}*}
+   ptopid="${udata%&*}"
+   sid="${udata#*&sid=}" #登录获取ptopid和sid
+   curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+   curl -d "@myvs2.txt" -d "jingdu=116.737&weidu=35.136&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+   udata=$(sed -n '24,26p' temp.txt)
+   echo "$udata" >> result.html]
   ;;
+*)
+echo "You didn't enter any parameter"
 esac
 done
