a38a0f444691b0feaa5cfe20bdfaf1c39cd7eded
di12ncj <94455250+di12ncj@users.noreply.github.com>
2022-01-05 18:17:19 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,7 +19,8 @@ case i in
   curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
   curl -d "@myvs1.txt" -d "jingdu=115.041&weidu=35.768&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
   udata=$(sed -n '24,26p' temp.txt)
-  echo "$udata" > result.html]
+  echo "$udata" > result.html
+  ]
   ;;
 1)[curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
    udata=$(sed -n '11p' temp.txt)
@@ -30,8 +31,9 @@ case i in
    curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
    curl -d "@myvs2.txt" -d "jingdu=116.737&weidu=35.136&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
    udata=$(sed -n '24,26p' temp.txt)
-   echo "$udata" >> result.html]
-  ;;
+   echo "$udata" >> result.html
+   ]
+   ;;
 *)
   echo "You didn't enter any parameter";;
 esac
