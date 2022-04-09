fc56bd7a22549093e5e9ea7f5eff78ff92ae4983
di12ncj <94455250+di12ncj@users.noreply.github.com>
2022-04-09 10:02:35 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,32 +9,14 @@ url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
 url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 for((i=0;i<num;i++))
 do
-case $i in
-0)
   curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
   udata=$(sed -n '11p' temp.txt)
   udata=${udata#*ptopid=}
   udata=${udata%\"\}\}*}
   ptopid="${udata%&*}"
   sid="${udata#*&sid=}" #登录获取ptopid和sid
-  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
-  curl -d "@myvs1.txt" -d "jingdu=113.6142&weidu=34.7595&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+  curl -d "day6=b&did=1&men6=a&fun18=819" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+  curl -d "@myvs1.txt" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
   udata=$(sed -n '24,26p' temp.txt)
   echo "$udata" > result.html
-  ;;
-1)
-  curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
-  udata=$(sed -n '11p' temp.txt)
-  udata=${udata#*ptopid=}
-  udata=${udata%\"\}\}*}
-  ptopid="${udata%&*}"
-  sid="${udata#*&sid=}" #登录获取ptopid和sid
-  curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
-  curl -d "@myvs2.txt" -d "jingdu=113.6142&weidu=34.7595&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
-  udata=$(sed -n '24,26p' temp.txt)
-  echo "$udata" >> result.html
-  ;;
-*)
-  echo "You didn't enter any parameter";;
-esac
 done
